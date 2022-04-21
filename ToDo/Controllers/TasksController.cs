f6d442eaac427182a09c994284399ccdc5f2f5c8
esrasa260 <esrasa260@gmail.com>
2022-04-21 16:56:25 +0300

    all files

--- ToDo/Controllers/TasksController.cs
+++ ToDo/Controllers/TasksController.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDo.Models.EntityFramework;
 
-namespace ToDo.Controllers
+namespace ToDoList.Controllers
 {
     public class TasksController : Controller
     {
-        // GET: Tasks
+        ToDoEntities db = new ToDoEntities();
         public ActionResult Index()
         {
-            return View();
+            var taskList = GetTasksList();
+            return View(taskList);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(string description)
+        {
+            Tasks task = new Tasks();
+            task.ID = Guid.NewGuid();
+            task.CREATIONDATE = DateTime.Now;
+            task.DESCRIPTION = description;
+            task.STATUS = "N";
+            db.Tasks.Add(task);
+            db.SaveChanges();
+
+            var taskList = GetTasksList();
+            return PartialView("_TasksList", taskList);
+        }
+
+        [HttpPost]
+        public ActionResult Complete(Guid id)
+        {
+
+            var task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            task.STATUS = "D";
+            db.SaveChanges();
+            var taskList = GetTasksList();
+            return PartialView("_TasksList", taskList);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(Guid id)
+        {
+            var task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            var taskList = GetTasksList();
+            return PartialView("_TasksList", taskList);
+        }
+
+        public List<Tasks> GetTasksList()
+        {
+            return db.Tasks.OrderByDescending(x => x.STATUS).ThenBy(x => x.CREATIONDATE).ToList();
         }
     }
 }
